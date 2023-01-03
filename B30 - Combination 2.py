1b8a5e436d6acbbd8803f6582cdf6b627a0f6841
sukenori <sukenori@coo.net>
2023-01-03 13:54:05 +0900

    B45まで

--- B30 - Combination 2.py
+++ B30 - Combination 2.py
@@ -1,12 +1,12 @@
-n,r=map(int,input().split())
+h,w=map(int,input().split())
 m=1000000007
-a=1
-for i in range(2,n+1):
+a=h-1+w-1 if h!=1 or w!=1 else 1
+for i in range(2,a):
     a=(a*i)%m
 b=1
-for i in range(2,r+1):
+for i in range(2,h):
     b=(b*i)%m
-for i in range(2,n-r+1):
+for i in range(2,w):
     b=(b*i)%m
 for _ in reversed(bin(m-2)[2:]):
     if _=="1": a=(a*b)%m
